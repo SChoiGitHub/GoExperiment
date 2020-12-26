5b4d884df45bcbf0d914b1cd35fcd047bc3042de
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2020-12-25 18:46:21 -0600

    Modified Dockerfile and server file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM golang AS builder
 RUN go get -u github.com/gorilla/mux
 ADD src/ .
-ADD internal/ /go/src/repository/
 EXPOSE 5000
-RUN GOOS=linux go build -mod=vendor -o /app .
+RUN GOOS=linux go build -o /app .
 CMD ["go", "run", "server.go"]
 
 FROM mongo
--- src/server.go
+++ src/server.go
@@ -15,10 +15,17 @@ func EchoHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Echo: ", params["message"])
 }
 
+func SaveHandler(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	message := params["message"];
+	fmt.Fprint(w, "Echo: ", message)
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", RootHandler)
 	router.HandleFunc("/echo/{message}", EchoHandler)
+	router.HandleFunc("/save/{message}", SaveHandler)
 
 	http.ListenAndServe(":5000", router)
 }
