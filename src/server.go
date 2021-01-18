ddc6c957846afd1ab3ef62eba6170dd9f1a110f7
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2021-01-17 20:52:34 -0600

    Implementing load handler

--- src/server.go
+++ src/server.go
@@ -1,68 +1,90 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"time"
-	"context"
+
 	"github.com/gorilla/mux"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second);
+var ctx, cancel = context.WithCancel(context.Background())
 
 func getClient() (*mongo.Client, func()) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	err = client.Connect(ctx)
 
-	if (err != nil) {
-		panic(err);
+	if err != nil {
+		panic(err)
 	}
 
 	disconnect := func() {
-		err = client.Disconnect(ctx);
-		if (err != nil) {
+		err = client.Disconnect(ctx)
+		if err != nil {
 			panic(err)
 		}
 	}
 
-	return client, disconnect;
+	return client, disconnect
 }
 
-func RootHandler(w http.ResponseWriter, r *http.Request){
+func RootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello!")
 }
 
-func EchoHandler(w http.ResponseWriter, r *http.Request){
+func EchoHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	fmt.Fprint(w, "Echo: ", params["message"])
 }
 
-func SaveHandler(w http.ResponseWriter, r *http.Request){
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	message := params["message"]
 
-	client, disconnect := getClient();
-	defer disconnect();
+	client, disconnect := getClient()
+	defer disconnect()
 	messageCollection := client.Database("testing").Collection("message")
 	res, err := messageCollection.InsertOne(ctx, bson.M{"value": message})
 
-	if (err != nil) {
-		panic(err);
+	if err != nil {
+		panic(err)
 	}
 
 	fmt.Fprint(w, "Saved: ", message, " at ", res.InsertedID)
 }
 
+func LoadHandler(w http.ResponseWriter, r *http.Request) {
+	var message interface{}
+
+	params := mux.Vars(r)
+	idHex := params["id"]
+
+	objId, err := primitive.ObjectIDFromHex(idHex)
+
+	client, disconnect := getClient()
+	defer disconnect()
+	messageCollection := client.Database("testing").Collection("message")
+	err = messageCollection.FindOne(ctx, bson.M{"_id": objId}).Decode(&message)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprint(w, "Message found: ", message)
+}
+
 func main() {
-	defer cancel();
-	
+	defer cancel()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", RootHandler)
 	router.HandleFunc("/echo/{message}", EchoHandler)
 	router.HandleFunc("/save/{message}", SaveHandler)
+	router.HandleFunc("/load/{id}", LoadHandler)
 
 	http.ListenAndServe(":5000", router)
 }
